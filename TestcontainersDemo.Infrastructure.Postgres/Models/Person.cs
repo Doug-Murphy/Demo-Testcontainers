5c4ca2fd6683fcb9d19da6f904cc95e17213207b
Doug Murphy <doug.murphy82@gmail.com>
2023-06-11 14:07:07 -0400

    Mark ref-type Person POCO properties as required

--- TestcontainersDemo.Infrastructure.Postgres/Models/Person.cs
+++ TestcontainersDemo.Infrastructure.Postgres/Models/Person.cs
@@ -3,9 +3,9 @@ namespace TestcontainersDemo.Infrastructure.Postgres.Models;
 public record Person {
     public Guid Id { get; set; }
 
-    public string FirstName { get; set; }
+    public required string FirstName { get; set; }
 
-    public string LastName { get; set; }
+    public required string LastName { get; set; }
 
     public DateOnly DateOfBirth { get; set; }
 }
