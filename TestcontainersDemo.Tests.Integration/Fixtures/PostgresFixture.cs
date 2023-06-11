4f7535e33ab8ed3ef8dadda95d62142b7d9c590f
Doug Murphy <doug.murphy82@gmail.com>
2023-06-11 18:34:47 -0400

    Create test project and Testcontainers fixture.

---
+++ TestcontainersDemo.Tests.Integration/Fixtures/PostgresFixture.cs
@@ -0,0 +1,17 @@
+﻿using Testcontainers.PostgreSql;
+
+namespace TestcontainersDemo.Tests.Integration.Fixtures; 
+
+public sealed class PostgresFixture : IAsyncLifetime {
+    private readonly PostgreSqlContainer _postgresContainer = new PostgreSqlBuilder()
+        .WithCleanUp(true)
+        .Build();
+    
+    public async Task InitializeAsync() {
+        await _postgresContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync() {
+        await _postgresContainer.StopAsync();
+    }
+}
